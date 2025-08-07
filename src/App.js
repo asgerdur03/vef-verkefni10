64c92b929c3ccf45e62c0c8695deac35fad1ba75
asgerdur03 <asgerdur03@gmail.com>
2025-08-07 22:07:56 +0000

    minor fixes

--- src/App.js
+++ src/App.js
@@ -12,25 +12,22 @@ function App() {
     const cap = caption.current.value;
 
     const url ='https://cataas.com/cat?json=true';
-    const getCatUrl = '';
-    
 
     try {
       const response = await fetch(url);
       const data = await response.json();
 
-      console.log(url)
+      console.log(data);
 
       if (!cap) {
-        const getCatUrl = `https://cataas.com/cat/${data._id}`;
+        const getCatUrl = `https://cataas.com/cat/${data.id}`;
         setCatImageUrl(getCatUrl);
         return;
       }else{
-        const getCatUrl = `https://cataas.com/cat/${data._id}/says/${cap}`;
+        const getCatUrl = `https://cataas.com/cat/${data.id}/says/${cap}`;
         setCatImageUrl(getCatUrl);
         
       }
-      console.log(getCatUrl);
 
     } catch (error) {
       console.log(error);
@@ -38,8 +35,7 @@ function App() {
 }
 
   return (
-    <div className="App">
-      <header className="App-header">
+      <>
         <h1>Verkefni 10 - Random cat with caption</h1>
         <p>
           Ýttu á Takka til þess að fá kattarmynd af handahófi. Ef þú vilt bæta við texta á mynd, skrifaðu textan í boxið, og ýttu svo á takkan. 
@@ -54,8 +50,7 @@ function App() {
             <button onClick={generate}>Generate</button>
           </div>
         </div>
-      </header>
-    </div>
+      </>
   );
 }
 
